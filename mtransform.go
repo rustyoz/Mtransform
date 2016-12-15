dbeec4d36bd83adbbbf021b43509cd9279e16379
Dave Hershberger <hersh@gmail.com>
2016-12-15 07:05:02 -0800

    Fixed MultiplyTransforms and added a test for it.

--- mtransform.go
+++ mtransform.go
@@ -25,13 +25,23 @@ func NewTransform() *Transform {
 }
 
 func MultiplyTransforms(a Transform, b Transform) Transform {
-	var t Transform
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			t[i][j] = t[i][j] + a[i][j]*b[j][i]
-		}
+	return Transform{
+		{
+			a[0][0]*b[0][0] + a[0][1]*b[1][0] + a[0][2]*b[2][0],
+			a[0][0]*b[0][1] + a[0][1]*b[1][1] + a[0][2]*b[2][1],
+			a[0][0]*b[0][2] + a[0][1]*b[1][2] + a[0][2]*b[2][2],
+		},
+		{
+			a[1][0]*b[0][0] + a[1][1]*b[1][0] + a[1][2]*b[2][0],
+			a[1][0]*b[0][1] + a[1][1]*b[1][1] + a[1][2]*b[2][1],
+			a[1][0]*b[0][2] + a[1][1]*b[1][2] + a[1][2]*b[2][2],
+		},
+		{
+			a[2][0]*b[0][0] + a[2][1]*b[1][0] + a[2][2]*b[2][0],
+			a[2][0]*b[0][1] + a[2][1]*b[1][1] + a[2][2]*b[2][1],
+			a[2][0]*b[0][2] + a[2][1]*b[1][2] + a[2][2]*b[2][2],
+		},
 	}
-	return t
 }
 
 func (a *Transform) MultiplyWith(b Transform) {
